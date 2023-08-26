34390e68e94af46b30f75169612277b77ab8c973
Sega <serega_7302@mail.ru>
2023-08-26 17:02:37 +0400

    Initial commit

---
+++ app/src/main/java/com/example/playground/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.playground
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button1: Button = findViewById(R.id.calculate_button)
+        button1.setOnClickListener {
+
+        }
+
+    }
+}
+
+
